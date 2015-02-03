f389f78e8d9ddd3b47e1849999d331271c70a77b
Sam Harwell <sam.harwell@rackspace.com>
2015-02-03 08:47:43 -0600

    Add initial code from template

---
+++ OpenStackNetAnalyzers/OpenStackNetAnalyzers/tools/install.ps1
@@ -0,0 +1,6 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$analyzerPath = join-path $toolsPath "analyzers"
+$analyzerFilePath = join-path $analyzerPath "OpenStackNetAnalyzers.dll"
+
+$project.Object.AnalyzerReferences.Add("$analyzerFilePath")
